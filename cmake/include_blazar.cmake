1ee9513e9987b13ff48c7e0f6f0d92cb89d06b7c
Murat Cengiz <cengizm9@gmail.com>
2022-05-17 15:04:07 +0200

    Support installing blazar engine as a library

--- cmake/include_blazar.cmake
+++ cmake/include_blazar.cmake
@@ -16,28 +16,31 @@ SET(SPVDir
         )
 
 
-SET(SPV
+SET(SPVHeaders
         ${SPVDir}/spirv.hpp
         ${SPVDir}/spirv_cfg.hpp
-        ${SPVDir}/spirv_cfg.cpp
         ${SPVDir}/spirv_cpp.hpp
-        ${SPVDir}/spirv_cpp.cpp
         ${SPVDir}/spirv_parser.hpp
-        ${SPVDir}/spirv_parser.cpp
         ${SPVDir}/spirv_reflect.hpp
-        ${SPVDir}/spirv_reflect.cpp
         ${SPVDir}/spirv_cross.hpp
-        ${SPVDir}/spirv_cross.cpp
         ${SPVDir}/spirv_cross_util.hpp
-        ${SPVDir}/spirv_cross_util.cpp
         ${SPVDir}/spirv_glsl.hpp
-        ${SPVDir}/spirv_glsl.cpp
         ${SPVDir}/spirv_cross_parsed_ir.hpp
+        )
+
+SET(SPVSources
+        ${SPVDir}/spirv_cfg.cpp
+        ${SPVDir}/spirv_cpp.cpp
+        ${SPVDir}/spirv_parser.cpp
+        ${SPVDir}/spirv_reflect.cpp
+        ${SPVDir}/spirv_cross.cpp
+        ${SPVDir}/spirv_cross_util.cpp
+        ${SPVDir}/spirv_glsl.cpp
         ${SPVDir}/spirv_cross_parsed_ir.cpp
         )
 
-ADD_SUBDIRECTORY("${CMAKE_SOURCE_DIR}/3DModelLoader/")
+ADD_SUBDIRECTORY("${PROJECT_SOURCE_DIR}/3DModelLoader/")
 
 INCLUDE_DIRECTORIES(
-        "${CMAKE_SOURCE_DIR}/3DModelLoader/src"
+        "${PROJECT_SOURCE_DIR}/3DModelLoader/src"
 )
