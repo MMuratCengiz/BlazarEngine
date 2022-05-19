e0a7633f66fb559af742651df9e95d567f1cc102
Murat Cengiz <cengizm9@gmail.com>
2022-05-19 19:43:26 +0200

    update boost

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
 #include <BlazarScene/World.h>
 #include <BlazarSamples/SampleGame_Small.h>
 
 using namespace BlazarEngine;
 using namespace ECS;
 
-
-int main( ) {
+int main( )
+{
     std::unique_ptr< Scene::World > world = std::make_unique< Scene::World >( );
     world->init( 1920, 1080, "Some Vulkan" );
 
     auto game = std::make_unique< Sample::SampleGame_Small >( world.get( ) );
-    world->run( game.get() );
+    world->run( game.get( ) );
 
     return 0;
 }
