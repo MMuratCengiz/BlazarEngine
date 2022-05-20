f5135e3d0e8aa469ba0fdda8d96fe7e8bee35bb8
Murat Cengiz <cengizm9@gmail.com>
2022-05-20 03:54:13 +0200

    minor cleanup

--- cmake/bullet_libs.cmake
+++ cmake/bullet_libs.cmake
@@ -1,35 +1,11 @@
 SET(BULLET_LIBS
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libBullet3Dynamics.a
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libBulletDynamics.a
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libBulletSoftBody.a
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libBullet3Geometry.a
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libBullet3Collision.a
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libBulletCollision.a
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libBulletInverseDynamics.a
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libLinearMath.a
-        ${PROJECT_BINARY_DIR}/bullet3/lib/libBullet3Common.a
+        Bullet3Dynamics
+        BulletDynamics
+        BulletSoftBody
+        Bullet3Geometry
+        Bullet3Collision
+        BulletCollision
+        BulletInverseDynamics
+        LinearMath
+        Bullet3Common
         )
-
-#SET(BULLET_LIBS
-#        Bullet3Common
-#        Bullet3Geometry
-#        BulletSoftBody
-#        BulletDynamics
-#        Bullet3Dynamics
-#        Bullet3Collision
-#        BulletCollision
-#        LinearMath
-#        BulletInverseDynamics
-#        )
-
-#SET(BULLET_LIBS
-#        Bullet3Dynamics
-#        BulletDynamics
-#        BulletSoftBody
-#        Bullet3Geometry
-#        Bullet3Collision
-#        BulletCollision
-#        BulletInverseDynamics
-#        Bullet3Common
-#        LinearMath
-#        )
